38907bb0e772638d65fe4e66a26e636ca510e4c4
Tyler <tyler_england35@yahoo.com>
2020-04-19 14:09:52 -0700

    Finished the POST request

--- sample_backend.py
+++ sample_backend.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 app = Flask(__name__)
 
 
@@ -35,16 +36,32 @@ users = {
 }
 
 
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST'])
 def get_users():
-    search_name = request.args.get('name')
-    if search_name :
-       subdict = {'user_list' : []}
-       for user in users['users_list']:
-          if user['name'] == search_name:
-             subdict['user_list'].append(user)
-       return subdict
-    return users
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      if search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      return resp
+
+
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           return user
+      return ({})
+   return users
 
 
 @app.route('/')
